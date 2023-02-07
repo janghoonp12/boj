6c1eced34e1045f131b7af66292b1b4d32189bbd
janghoonp12 <75202505+janghoonp12@users.noreply.github.com>
2023-02-08 00:35:09 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 252 ms, Memory: 129592 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -1,18 +1,13 @@
-def pfx(n, arr):
-    prefix = [0 for i in range(n + 1)]
-    for i in range(1, n + 1):
-        prefix[i] = prefix[i - 1] + arr[i]
-    return prefix
+from sys import stdin
+I = stdin.readline
 
-def function(n, m, arr, arr2):
-    arr = [0] + arr
-    prefix = pfx(n, arr)
-    for i, j in arr2:
-        print(prefix[j] - prefix[i - 1])
-    return
+N, M = map(int, I().split())
+arr = [0] + list(map(int, I().split()))
+prefix = [0]*(N+1)
 
+for i in range(1, N+1):
+    prefix[i] = arr[i] + prefix[i-1]
 
-n, m = map(int, input().split())
-arr = list(map(int, input().split()))
-arr2 = [list(map(int, input().split())) for i in range(m)]
-function(n, m, arr, arr2)
+for _ in range(M):
+    i, j = map(int, I().split())
+    print(prefix[j]-prefix[i-1])
