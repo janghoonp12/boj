abd851f7c9902230989dc5130f2ff44a66baaf68
janghoonp12 <75202505+janghoonp12@users.noreply.github.com>
2023-03-27 21:03:30 +0900

    [Silver II] Title: 부분수열의 합, Time: 244 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -0,0 +1,20 @@
+def recur(cur, total):
+    global cnt
+    
+    if cur == n:
+        if total == s:
+            cnt += 1
+        return
+    
+    recur(cur + 1, total + arr[cur])
+    recur(cur + 1, total)
+
+
+n, s = map(int, input().split())
+arr = list(map(int, input().split()))
+cnt = 0
+if s == 0:
+    cnt -= 1
+
+recur(0, 0)
+print(cnt)
