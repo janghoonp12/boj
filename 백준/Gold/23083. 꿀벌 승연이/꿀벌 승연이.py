5c2a33ad0399f9a68f01a557becb650fb6ca08e0
janghoonp12 <75202505+janghoonp12@users.noreply.github.com>
2023-04-14 02:19:44 +0900

    [Gold V] Title: 꿀벌 승연이, Time: 2432 ms, Memory: 270936 KB -BaekjoonHub

--- 백준/Gold/23083. 꿀벌 승연이/꿀벌 승연이.py
+++ 백준/Gold/23083. 꿀벌 승연이/꿀벌 승연이.py
@@ -1,12 +1,12 @@
 n, m = map(int, input().split())
 k = int(input())
-holes = [[1 for i in range(m + 1)] for j in range(n + 1)]
+holes = [[False for i in range(m + 1)] for j in range(n + 1)]
 for i in range(k):
     x, y = map(int, input().split())
-    holes[x][y] = 0
+    holes[x][y] = True
 
-dx = [[-1, 0, 1], [-1, -1, 0]]
-dy = [[0, -1, -1], [0, -1, -1]]
+dx = [[0, 1, 1], [-1, 0, 1]]
+dy = [[1, 1, 0], [1, 1, 0]]
 
 arr = [[0 for i in range(m + 1)] for j in range(n + 1)]
 arr[1][1] = 1
@@ -15,9 +15,8 @@ for y in range(1, m + 1):
         for k in range(3):
             nx = x + dx[y % 2][k]
             ny = y + dy[y % 2][k]
-            if not (nx <= n and ny <= m):
+            if not (0 < nx <= n and  0 < ny <= m) or holes[nx][ny]:
                 continue
-
-            arr[x][y] += holes[nx][ny] * arr[nx][ny]
+            arr[nx][ny] += arr[x][y]
 
 print(arr[n][m] % (10**9 + 7))
