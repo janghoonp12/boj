28aa262c17854b5cc3551140d96a7abc34ad20fd
janghoonp12 <75202505+janghoonp12@users.noreply.github.com>
2023-04-14 02:27:55 +0900

    [Gold V] Title: 꿀벌 승연이, Time: 1120 ms, Memory: 381368 KB -BaekjoonHub

--- 백준/Gold/23083. 꿀벌 승연이/꿀벌 승연이.py
+++ 백준/Gold/23083. 꿀벌 승연이/꿀벌 승연이.py
@@ -3,22 +3,24 @@ input = stdin.readline
 
 n, m = map(int, input().split())
 k = int(input())
-holes = [[False for i in range(m + 1)] for j in range(n + 1)]
+
+arr = [[0 for i in range(m + 1)] for j in range(n + 1)]
 for i in range(k):
     x, y = map(int, input().split())
-    holes[x][y] = True
+    arr[x][y] = -1
+arr[1][1] = 1
 
 dx = [[0, 1, 1], [-1, 0, 1]]
 dy = [[1, 1, 0], [1, 1, 0]]
 
-arr = [[0 for i in range(m + 1)] for j in range(n + 1)]
-arr[1][1] = 1
 for y in range(1, m + 1):
     for x in range(1, n + 1):
+        if arr[x][y] == -1:
+            continue
         for k in range(3):
             nx = x + dx[y % 2][k]
             ny = y + dy[y % 2][k]
-            if not (0 < nx <= n and  0 < ny <= m) or holes[nx][ny]:
+            if not (0 < nx <= n and  0 < ny <= m) or arr[nx][ny] == -1:
                 continue
             arr[nx][ny] += arr[x][y]
 
